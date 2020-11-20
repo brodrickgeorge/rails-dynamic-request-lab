98f6cd997edc161fad52b9cc805a045a989b6a38
brodrickgeorge <brodrick.george@yahoo.com>
2020-11-20 13:41:27 -0600

    commit tree ce03e50b6fc6351eaa5e2a5be5752996e4e93a0b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,12 @@
 class StudentsController < ApplicationController
+  before_action :student, only: :show
+
   def index
     @students = Student.all
   end
+
+  def student
+    @student = Student.find(params[:id])
+  end
+
 end
